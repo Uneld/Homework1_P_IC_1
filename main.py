5ee360c850c6fd8940bb5ed8dfc18e6c6deadf35
Uneld <aleksey.minchenya@gmail.com>
2023-09-11 21:35:41 +0300

    added notes editing function, added check for not found note number when deleting fixed bug with date recording, added date output in notes view

--- main.py
+++ main.py
@@ -11,10 +11,11 @@ def add_note():
     """
     title = input('Введите заголовок заметки: ')
     note_body = input('Введите текст заметки: ')
-    now_date = datetime.datetime.now().strftime('%H:%M:%S %d-%m-%Y'),
+    now_date = datetime.datetime.now().strftime('%H:%M:%S %d-%m-%Y')
+    print(now_date)
     note = {
         'id': len(notes) + 1,
-        'title': title,
+        'note_title': title,
         'note_body': note_body,
         'create_date': now_date,
         'update_date': now_date
@@ -29,8 +30,9 @@ def show_all_notes():
     if notes:
         for note in notes:
             print(f'\n******** Номер: {note["id"]} ********')
-            print(f'Заголовок: {note["title"]}')
+            print(f'Заголовок: {note["note_title"]}')
             print(f'{note["note_body"]}')
+            print(f'Последнее изменение: {note["update_date"]}')
             print('****************************\n')
 
     else:
@@ -41,29 +43,60 @@ def show_all_notes():
 
 def delete_note():
     """
-    Запрашивает у пользователя идентификатор заметки, удаляет заметку из списка notes
+    Запрашивает у пользователя номер заметки, удаляет заметку из списка notes,
+    если не найдено выводит сообщение
     """
     note_id = int(input('Введите номер заметки: '))
     global notes
-    new_notes = [note for note in notes if note['id'] != note_id]
-    notes = new_notes
+    found_note = False
+    new_notes = []
+    for note in notes:
+        if note['id'] == note_id:
+            found_note = True
+        else:
+            new_notes.append(note)
+    if found_note:
+        notes = new_notes
+    else:
+        print(f'\nЗаметка с номером: {note_id} не найдена\n')
+
+
+def edit_note():
+    """
+    Запрашивает у пользователя номер заметки, находит заметку в списке notes,
+    запрашивает у пользователя новый заголовок и текст заметки, обновляет заметку с новой информацией
+    и временными метками.
+    """
+    note_id = int(input('Введите номер заметки: '))
+    for note in notes:
+        if note['id'] == note_id:
+            title = input('Введите новый заголовок заметки: ')
+            body = input('Введите новый текст заметки: ')
+            note['note_title'] = title
+            note['note_body'] = body
+            note['update_date'] = datetime.datetime.now().strftime('%H:%M:%S %d-%m-%Y')
+            return
+    print(f'\nЗаметка с номером: {note_id} не найдена\n')
 
 
 while flag_work:
     print('1. Добавить заметку')
-    print('2. Удалить заметку')
-    print('3. Просмотреть все заметки')
-    print('4. Выход')
+    print('2. Редактировать заметку')
+    print('3. Удалить заметку')
+    print('4. Просмотреть все заметки')
+    print('5. Выход')
 
     choice = input('Введите номер команды: ')
 
     if choice == '1':
         add_note()
     elif choice == '2':
-        delete_note()
+        edit_note()
     elif choice == '3':
-        show_all_notes()
+        delete_note()
     elif choice == '4':
+        show_all_notes()
+    elif choice == '5':
         flag_work = False
     else:
         print('Неверный номер команды')
